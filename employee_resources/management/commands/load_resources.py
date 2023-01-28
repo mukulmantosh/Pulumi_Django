13d6818170ef2c59b026017754df74b717ecefb9
Mukul Mantosh <mukulmantosh@yahoo.com>
2023-01-28 20:28:01 +0530

    Feat: WIP

--- employee_resources/management/commands/load_resources.py
+++ employee_resources/management/commands/load_resources.py
@@ -1,15 +1,35 @@
+import boto3
 from django.core.management.base import BaseCommand
 
 from employee_resources.management.commands import vpc, subnets
 
+
+def ec2_instance_types(region_name):
+    '''Yield all available EC2 instance types in region <region_name>'''
+    ec2 = boto3.client('ec2', region_name=region_name)
+    describe_args = {}
+    while True:
+        describe_result = ec2.describe_instance_types(**describe_args)
+        yield from [i['InstanceType'] for i in describe_result['InstanceTypes']]
+        if 'NextToken' not in describe_result:
+            break
+        describe_args['NextToken'] = describe_result['NextToken']
 
 class Command(BaseCommand):
     help = 'Load AWS Resource Information into DB'
 
     def handle(self, *args, **kwargs):
         self.stdout.write("Dumping AWS resources into database...")
-        vpc.retrieve_vpc_info(self)
-        subnets.retrieve_subnet_info(self)
+        # vpc.retrieve_vpc_info(self)
+        # subnets.retrieve_subnet_info(self)
+
+        # for ec2_type in ec2_instance_types('ap-south-1'):
+        #     print(ec2_type)
+
+        ec2_client = boto3.client('ec2', region_name='ap-southeast-1')  # Change as appropriate
+        images = ec2_client.describe_images(Owners=['self'])
+        print(images)
+
 
 
         # ec2 = boto3.client('ec2')
